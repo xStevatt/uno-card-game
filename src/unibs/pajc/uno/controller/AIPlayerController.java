c2329ce659a1503da66f8b8d140ccccab1f8f6be
Stefano Valloncini <stefanovalloncini@protonmail.com>
2022-06-19 14:02:51 +0200

    Update

--- src/unibs/pajc/uno/controller/AIPlayerController.java
+++ src/unibs/pajc/uno/controller/AIPlayerController.java
@@ -2,6 +2,7 @@ package unibs.pajc.uno.controller;
 
 import unibs.pajc.uno.controller.ai.AI;
 import unibs.pajc.uno.model.GameModel;
+import unibs.pajc.uno.model.player.Player;
 import unibs.pajc.uno.view.TableView;
 
 public class AIPlayerController
@@ -19,5 +20,21 @@ public class AIPlayerController
 	public void initView()
 	{
 		view.setVisible(true);
+		view.setResizable(false);
+		view.setTitle("Single player game");
+	}
+
+	public void initModel()
+	{
+		Player playerOne = new Player("Human", model.generateStartingCards(), 0);
+		Player playerTwo = new Player("Robot", model.generateStartingCards(), 1);
+
+		model.initPlayer(playerOne);
+		model.initPlayer(playerTwo);
+	}
+
+	public void updateView()
+	{
+
 	}
 }
--- src/unibs/pajc/uno/controller/ai/AI.java
+++ src/unibs/pajc/uno/controller/ai/AI.java
@@ -1,9 +1,12 @@
 package unibs.pajc.uno.controller.ai;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import unibs.pajc.uno.model.GameModel;
 import unibs.pajc.uno.model.card.Card;
+import unibs.pajc.uno.model.card.CardColor;
 import unibs.pajc.uno.model.player.Player;
 
 public class AI
@@ -16,13 +19,42 @@ public class AI
 		this.gameModel = gameModel;
 	}
 
+	private Card hasSpecialWildCard(ArrayList<Card> cardsList)
+	{
+		cardsList.stream().filter(e -> e.isCardSpecialWild());
+
+		CardColor cardColor = determineMostPresentColor(cardsList);
+
+		if (!cardList.isEmpty())
+			return cardList.get((int) (Math.random() * cardList.size()));
+
+		return null;
+	}
+
+	private CardColor determineMostPresentColor(ArrayList<Card> cardsList)
+	{
+		HashMap<CardColor, Integer> hashColorMap = new HashMap<CardColor, Integer>();
+
+		for (Card card : cardList)
+		{
+			hashColorMap.put(card.getCardColor(), hashColorMap.get(card.getCardColor()) + 1);
+		}
+
+		for (Map.Entry entry : hashColorMap.entrySet())
+		{
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		}
+
+		return CardColor.RED;
+	}
+
 	public Card determineNexMossa()
 	{
 		Player currentPlayer = gameModel.getCurrentPlayer();
 		ArrayList<Card> playerCardList = currentPlayer.getHandCards().getCardList();
 
 		// CHECKS IF NEXT ADVERSARY HAS MORE CARDS THAN CURRENT PLAYER
-		boolean hasPlayerMoreCards = gameModel.getCurrentPlayer().getHandCards().getCardList().size() > gameModel
+		boolean hasPlayerMoreCards = gameModel.getCurrentPlayer().getHandCards().getCardList().size() < gameModel
 				.getPlayers().get(gameModel.getNextPlayerIndex()).getHandCards().getCardList().size();
 
 		// FILTERS OUT ONLY VALID CARDS
@@ -31,7 +63,15 @@ public class AI
 
 		if (playerCardList.size() != 0)
 		{
+			if (!hasPlayerMoreCards && hasSpecialWildCard(cardList) != null)
+			{
+				System.out.println("Player ahs special card list");
+			}
 
+			else if (!hasPlayerMoreCards && hasSpecialWildCard(cardList) != null)
+			{
+				System.out.println("Player has not special cards list");
+			}
 		}
 
 		// RETURNS A NEW CARD
