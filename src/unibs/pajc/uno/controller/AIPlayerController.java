dfbffc0582e524e45e4c88e38e41e999d064c1aa
Stefano Valloncini <stefanovalloncini@protonmail.com>
2022-06-19 14:11:19 +0200

    Update

--- src/unibs/pajc/uno/controller/AIPlayerController.java
+++ src/unibs/pajc/uno/controller/AIPlayerController.java
@@ -1,11 +1,14 @@
 package unibs.pajc.uno.controller;
 
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import unibs.pajc.uno.controller.ai.AI;
 import unibs.pajc.uno.model.GameModel;
 import unibs.pajc.uno.model.card.Card;
 import unibs.pajc.uno.model.player.Player;
+import unibs.pajc.uno.view.CardView;
 import unibs.pajc.uno.view.TableView;
 import unibs.pajc.uno.view.events.CardDrawnEvent;
 import unibs.pajc.uno.view.events.CardSelectedEvent;
@@ -25,31 +28,24 @@ public class AIPlayerController
 	private CardSelectedEvent mouseListener;
 	private CardDrawnEvent mouseListenerDrawnCard;
 
-	public AIPlayerController()
+	public AIPlayerController(String nameHuman1)
 	{
-		view = new TableView("", "", true);
+		// INITS MODEL
+		model = new GameModel();
+		initModel(nameHuman1, "Robot");
+
+		// INITS VIEW
+		view = new TableView(nameHuman1, "Robot", true);
 		initView();
+
+		// START RUNNING GAME
+		executor = Executors.newCachedThreadPool();
+		executor.execute(this::runGame);
 	}
 
 	public void initView()
 	{
-		view.setVisible(true);
-		view.setResizable(false);
-		view.setTitle("Single player game");
-	}
-
-	public void initModel()
-	{
-		Player playerOne = new Player("Human", model.generateStartingCards(), 0);
-		Player playerTwo = new Player("Robot", model.generateStartingCards(), 1);
-
-		model.initPlayer(playerOne);
-		model.initPlayer(playerTwo);
-
-	}
-
-	public void updateView()
-	{
+		// SHOWS GUI
 		view.setVisible(true);
 		view.setResizable(false);
 		view.setLocationRelativeTo(null);
@@ -59,5 +55,56 @@ public class AIPlayerController
 		view.getCardDeckView().addMouseListener(mouseListenerDrawnCard);
 
 		updateView();
+	}
+
+	public void initModel(String namePlayerOne, String namePlayerTwo)
+	{
+		Player playerOne = new Player("Human", model.generateStartingCards(), 0);
+		Player playerTwo = new Player("Robot", model.generateStartingCards(), 1);
+
+		model.initPlayer(playerOne);
+		model.initPlayer(playerTwo);
+
+		ai = new AI(model);
+	}
+
+	public void updateView()
+	{
+
+		// SETS TURN AND LOADS CARDS
+		view.setTurn(model.getCurrentPlayer().getNamePlayer());
+		view.loadCards(model.getPlayers().get(0).getHandCards(), 0);
+		view.loadCards(model.getPlayers().get(1).getHandCards(), 1);
+		view.changeDroppedCardView(model.getLastCardUsed(), model.getCurrentCardColor());
+
+		// DISABLE BUTTONS
+		view.setSayUnoButtonVisibile(false, model.getPreviousPlayerIndex());
+		view.setSayUnoButtonVisibile(false, model.getCurrentPlayerIndex());
+
+		// GETS ALL CARDS VIEWS FROM GAMEVIEW
+		ArrayList<CardView> panelPlayerOneCards = view.getAllCards(0,
+				model.getPlayers().get(0).getHandCards().getNumberOfCards());
+		ArrayList<CardView> panelPlayerTwoCards = view.getAllCards(1,
+				model.getPlayers().get(1).getHandCards().getNumberOfCards());
+
+		// ADD MOUSE LISTENERS TO CARDS
+		mouseListener = new CardSelectedEvent(notifyObj);
+		panelPlayerOneCards.forEach(e -> e.addMouseListener(mouseListener));
+		panelPlayerTwoCards.forEach(e -> e.addMouseListener(mouseListener));
+
+		// ENABLES / DISABLE VIEW FOR PLAYERS
+		view.enableViewPlayer(model.getCurrentPlayerIndex(), true);
+		view.enableViewPlayer(model.getNextPlayerIndex(), false);
+
+		// CHECKS IF UNO BUTTON SHOULD BE ENABLED
+		view.setSayUnoButtonVisibile(model.hasPlayerOneCard(model.getCurrentPlayer()), model.getCurrentPlayerIndex());
+
+		// REPAINTS VIEW
+		view.repaint();
+	}
+
+	public void runGame()
+	{
+
 	}
 }
